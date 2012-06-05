9cdecaf5a752fdda484a24ddd86f22fe8a0a5b5d
Saptarshi Purkayastha <sunbiz@gmail.com>
2012-06-05 07:31:08 +0000

    RESTWS-248 - ProviderResource and ProviderController. Work in Progress

--- omod/src/test/java/org/openmrs/module/webservices/rest19ext/test/Rest19ExtTestConstants.java
+++ omod/src/test/java/org/openmrs/module/webservices/rest19ext/test/Rest19ExtTestConstants.java
@@ -2,11 +2,15 @@ package org.openmrs.module.webservices.rest19ext.test;
 
 public class Rest19ExtTestConstants {
 	
-	public final static String LOCATION_UUID = "f08ba64b-ea57-4a41-b33c-9dfc59b0c60a";
+	public final static String LOCATION_UUID = "f08ba64b-ea57-4a41-nfdr-9dfc59b0c60a";
 	
 	public final static String LOCATION_ATTRIBUTE_UUID = "3a2bdb18-6faa-11e0-8414-001e378eb67e";
 	
 	public final static String LOCATION_ATTRIBUTE_TYPE_UUID = "9516cc50-6f9f-11e0-8414-001e378eb67e";
+	
+	public final static String LOCATION_ATTRIBUTE_TYPE2_UUID = "9516cc50-6f9f-132r-5433-001e378eb67f";
+	
+	public final static String PROVIDER_UUID = "c2299800-dgha-11e0-9572-0800200c9a66";
 	
 	public final static String TEST_DATASET = "customTestDataset.xml";
 }
